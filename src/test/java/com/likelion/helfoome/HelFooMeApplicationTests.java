7b1c6f41d4951bf258ac349bf76464a776c15105
YoonHeeJun <unijun0109@gmail.com>
2024-10-16 03:04:56 +0900

    :tada: Start: Initial Setting

---
+++ src/test/java/com/likelion/helfoome/HelFooMeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.likelion.helfoome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelFooMeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
