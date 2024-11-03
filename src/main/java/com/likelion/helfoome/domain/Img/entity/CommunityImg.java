38355cbf9d71b97afe1ed9c6ee374cf1632f7bf9
s0obang <tnqls40168@gmail.com>
2024-11-04 01:18:58 +0900

    :sparkles: Feat: Image Upload

--- src/main/java/com/likelion/helfoome/domain/Img/entity/CommunityImg.java
+++ src/main/java/com/likelion/helfoome/domain/Img/entity/CommunityImg.java
@@ -1,11 +1,15 @@
 package com.likelion.helfoome.domain.Img.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+import com.likelion.helfoome.domain.post.entity.Community;
 import com.likelion.helfoome.global.common.BaseTimeEntity;
 
 import lombok.Getter;
@@ -16,9 +20,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "articleImg")
+@Table(name = "communityImg")
 public class CommunityImg extends BaseTimeEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "communityId", referencedColumnName = "id", nullable = false)
+  private Community community;
+
+  @Column(name = "communityImageName", nullable = false)
+  private String communityImageName;
+
+  @Column(name = "communityImageUrl", nullable = false)
+  private String communityImageUrl;
 }
