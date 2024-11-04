8ef3f16c962ed4d99243e1aa551a790f2120b6ef
HeeJun Yoon <118972548+uni-j-uni@users.noreply.github.com>
2024-11-04 19:22:24 +0900

    ♻️ Refactor: Api 경로 권한 부여
    Api 경로 권한 부여

--- src/main/java/com/likelion/helfoome/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/helfoome/global/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.likelion.helfoome.global.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,8 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.likelion.helfoome.domain.user.service.UserService;
 import com.likelion.helfoome.global.auth.handler.CustomSuccessHandler;
@@ -33,7 +30,25 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, UserService userService)
       throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http.cors(
+            corsCustomizer ->
+                corsCustomizer.configurationSource(
+                    request -> {
+                      CorsConfiguration configuration = new CorsConfiguration();
+
+                      configuration.setAllowedOrigins(
+                          Collections.singletonList("http://localhost:3000"));
+                      configuration.setAllowedMethods(Collections.singletonList("*"));
+                      configuration.setAllowCredentials(true);
+                      configuration.setAllowedHeaders(Collections.singletonList("*"));
+                      configuration.setMaxAge(3600L);
+
+                      configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                      configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                      return configuration;
+                    }))
+        .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         // Bearer 방식을 사용할건데 스프링 시큐리티는 httpBasic 이 잡혀있음
         .httpBasic(AbstractHttpConfigurer::disable)
@@ -44,11 +59,8 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             request ->
                 request
-                    .requestMatchers("/",
-                        "/swagger-ui/**",
-                        "/api/users/**",
-                        "/api/shop/**",
-                        "/api/product/**")
+                    .requestMatchers(
+                        "/", "/swagger-ui/**", "/api/users/**", "/api/shop/**", "/api/product/**")
                     .permitAll()
                     .requestMatchers("/api/v1/user/*")
                     .hasRole("USER || SELLER")
@@ -65,29 +77,8 @@ public class SecurityConfig {
                     .userInfoEndpoint(
                         (userInfoEndpointConfig) ->
                             userInfoEndpointConfig.userService(customOAuth2UserService))
-                    .successHandler(customSuccessHandler))
-        .cors(Customizer.withDefaults());
+                    .successHandler(customSuccessHandler));
 
     return http.build();
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(
-      AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.addAllowedOriginPattern("*"); // 모든 도메인 허용
-    configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-    configuration.addAllowedHeader("*"); // 모든 헤더 허용
-    configuration.setAllowCredentials(true); // 자격 증명 허용
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-
-    return source;
   }
 }
