3e5d43b9e381e90f501264e986b813916c9e5785
s0obang <tnqls40168@gmail.com>
2024-11-05 03:24:12 +0900

    :sparkles: Feat: get Distance

---
+++ src/main/java/com/likelion/helfoome/global/auth/dto/CoordinateDto.java
@@ -0,0 +1,14 @@
+package com.likelion.helfoome.global.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CoordinateDto {
+
+  private String x;
+  private String y;
+  private String error;
+
+}
