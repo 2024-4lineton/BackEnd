882d410df37eb1ac7bec6f123e07bcae8a7de8d1
uni-j-uni <unijun0109@gmail.com>
2024-11-05 20:35:45 +0900

    :recycle: Refactor:ã 코드ã Code refactoring

--- src/main/java/com/likelion/helfoome/domain/shop/entity/Product.java
+++ src/main/java/com/likelion/helfoome/domain/shop/entity/Product.java
@@ -2,6 +2,7 @@ package com.likelion.helfoome.domain.shop.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class Product extends BaseTimeEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "shopId", referencedColumnName = "id", nullable = false)
   private Shop shop;
 
