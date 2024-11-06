f4c20ee10ed0c1a4f8844515c2617ebfb7076810
uni-j-uni <unijun0109@gmail.com>
2024-11-07 02:32:15 +0900

    :recycle: Refactor: Content 추가

---
+++ src/main/java/com/likelion/helfoome/domain/post/dto/CommentRequest.java
@@ -0,0 +1,12 @@
+package com.likelion.helfoome.domain.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+  private String content;
+}
