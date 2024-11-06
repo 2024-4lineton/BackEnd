27d722bb47ec8cf2ea5a5bed496338f75530c532
uni-j-uni <unijun0109@gmail.com>
2024-11-07 02:58:44 +0900

    :sparkles: Feat: 좋아요 중복 불가 기능 추가

--- src/main/java/com/likelion/helfoome/domain/post/repository/CommunityLikeRepository.java
+++ src/main/java/com/likelion/helfoome/domain/post/repository/CommunityLikeRepository.java
@@ -1,9 +1,13 @@
 package com.likelion.helfoome.domain.post.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.likelion.helfoome.domain.post.entity.CommunityLike;
 
 @Repository
-public interface CommunityLikeRepository extends JpaRepository<CommunityLike, Long> {}
+public interface CommunityLikeRepository extends JpaRepository<CommunityLike, Long> {
+  Optional<CommunityLike> findByCommunityIdAndUser_Email(Long communityId, String email);
+}
