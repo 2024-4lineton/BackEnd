3a4970b694db4b7a7f36dd081d1b5c8d07c95879
uni-j-uni <unijun0109@gmail.com>
2024-11-09 18:26:01 +0900

    :recycle: Refactor: 연관관계 변경

--- src/main/java/com/likelion/helfoome/domain/user/entity/User.java
+++ src/main/java/com/likelion/helfoome/domain/user/entity/User.java
@@ -1,5 +1,8 @@
 package com.likelion.helfoome.domain.user.entity;
 
+import com.likelion.helfoome.domain.cart.entity.Cart;
+import com.likelion.helfoome.domain.cart.entity.CartProduct;
+import com.likelion.helfoome.domain.shop.entity.Shop;
 import jakarta.persistence.*;
 
 import com.likelion.helfoome.global.common.BaseTimeEntity;
@@ -29,4 +32,22 @@ public class User extends BaseTimeEntity {
 
   @Column(name = "role", nullable = false)
   private String userRole = "ROLE_USER";
+
+  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+  @JoinColumn(name = "userInfo_id")
+  private UserInfo userInfo;
+
+  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+  @JoinColumn(name = "cart_id")
+  private Cart cart;
+
+  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+  @JoinColumn(name = "cartProduct_id")
+  private CartProduct cartProduct;
+
+  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+  @JoinColumn(name = "shop_id")
+  private Shop shop;
+
+
 }
