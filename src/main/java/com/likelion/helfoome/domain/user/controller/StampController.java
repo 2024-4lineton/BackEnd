437e83c9542deff08dfdde33a05f5867deb01080
s0obang <tnqls40168@gmail.com>
2024-11-10 23:53:02 +0900

    :sparkles: Feat: 알림 api명세

--- src/main/java/com/likelion/helfoome/domain/user/controller/StampController.java
+++ src/main/java/com/likelion/helfoome/domain/user/controller/StampController.java
@@ -1,19 +1,18 @@
 package com.likelion.helfoome.domain.user.controller;
 
+import com.likelion.helfoome.domain.user.dto.StampResponse;
+import com.likelion.helfoome.domain.user.service.StampService;
+import com.likelion.helfoome.global.auth.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.likelion.helfoome.domain.user.dto.StampResponse;
-import com.likelion.helfoome.domain.user.service.StampService;
-import com.likelion.helfoome.global.auth.jwt.JwtUtil;
-
-import io.jsonwebtoken.Claims;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
@@ -24,6 +23,7 @@ public class StampController {
   private final JwtUtil jwtUtil;
   private final StampService stampService;
 
+  @Operation(summary = "스탬프 가져오기", description = "요일별 스탬프 여부 반환")
   @GetMapping
   public ResponseEntity<StampResponse> getStamp(
       @RequestHeader("Authorization") String bearerToken) {
