689d3fa6d0a99aa113328afa37f74c6218f7e665
s0obang <tnqls40168@gmail.com>
2024-11-12 19:52:09 +0900

    :bug: Fix: 순환참조 방지.. 최대한 보존하는 방향으~로

--- src/main/java/com/likelion/helfoome/domain/shop/dto/product/ProductManagingResponse.java
+++ src/main/java/com/likelion/helfoome/domain/shop/dto/product/ProductManagingResponse.java
@@ -1,13 +1,13 @@
 package com.likelion.helfoome.domain.shop.dto.product;
 
 import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProductManagingResponse {
-
 
   private ProductResponse productResponse;
   private List<OrderInList> orders;
