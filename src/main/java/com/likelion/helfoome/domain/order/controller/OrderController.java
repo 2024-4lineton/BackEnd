3dc2cf63ca87f140a205d3e2bbc94414855e654d
YoonHeeJun <unijun0109@gmail.com>
2024-11-13 21:33:46 +0900

    :sparkles: Feat: 닉네임 중복 확인 기능

--- src/main/java/com/likelion/helfoome/domain/order/controller/OrderController.java
+++ src/main/java/com/likelion/helfoome/domain/order/controller/OrderController.java
@@ -1,13 +1,7 @@
 package com.likelion.helfoome.domain.order.controller;
 
-import com.likelion.helfoome.domain.order.dto.OrderCompleteList;
-import com.likelion.helfoome.domain.order.service.OrderService;
-import com.likelion.helfoome.global.auth.jwt.JwtUtil;
-import io.jsonwebtoken.Claims;
-import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +11,15 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.likelion.helfoome.domain.order.dto.OrderCompleteList;
+import com.likelion.helfoome.domain.order.service.OrderService;
+import com.likelion.helfoome.global.auth.jwt.JwtUtil;
+
+import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
